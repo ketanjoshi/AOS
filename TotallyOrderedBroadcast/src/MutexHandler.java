165553e10b94bc89c774647bbe15edc93f0334d4
snehamvp <snehamvp@gmail.com>
2015-11-29 22:29:02 -0600

    Changed signature of CSEnter and CSLeave

--- TotallyOrderedBroadcast/src/MutexHandler.java
+++ TotallyOrderedBroadcast/src/MutexHandler.java
@@ -1,25 +1,25 @@
 
 public class MutexHandler {
 	
-	public void csEnter(ClusterNode clusterNode)
+	public void csEnter()
 	{
-		MutexPriorityQueueElement qElement = new MutexPriorityQueueElement(clusterNode.getId(), clusterNode.getLogicalClock());
+		MutexPriorityQueueElement qElement = new MutexPriorityQueueElement(MutexGlobals.id, TobMutexGlobals.getLogicalclock());
 		MutexGlobals.addNodeToPriorityQueue(qElement);
-		MutexGlobals.setMutexReqClock(clusterNode.getLogicalClock());
+		MutexGlobals.setMutexReqClock(TobMutexGlobals.getLogicalclock());
 		
-		Message msg = new Message(clusterNode.getId(), null, MessageType.MUTEX_REQUEST, clusterNode.getLogicalClock());
+		Message msg = new Message(MutexGlobals.id, null, MessageType.MUTEX_REQUEST, TobMutexGlobals.getLogicalclock());
 		
 		//Initiate sender thread to broadcast the mutex request message
 		LamportsSender lamportSender = new LamportsSender(msg,null);
 		Thread thread = new Thread(lamportSender);
         thread.start();
 	}
-	public void csLeave(ClusterNode clusterNode)
+	public void csLeave()
 	{
-		MutexPriorityQueueElement qElement = new MutexPriorityQueueElement(clusterNode.getId(), clusterNode.getLogicalClock());
+		MutexPriorityQueueElement qElement = new MutexPriorityQueueElement(MutexGlobals.id, TobMutexGlobals.getLogicalclock());
 		MutexGlobals.removeNodeFromPriorityQueue(qElement);
 		
-		Message msg = new Message(clusterNode.getId(), null, MessageType.CS_LEAVE, clusterNode.getLogicalClock());
+		Message msg = new Message(MutexGlobals.id, null, MessageType.CS_LEAVE, TobMutexGlobals.getLogicalclock());
 		
 		//Initiate sender thread to broadcast the mutex leave message
 		LamportsSender lamportSender = new LamportsSender(msg,null);
